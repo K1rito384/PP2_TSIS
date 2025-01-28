1129dffb2196132585e495a8046281120f6a15eb
K1rito384 <akeshubaev2006@gmail.com>
2025-01-28 14:13:17 +0500

    new commit

---
+++ TSIS3/Classes/task5.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposited {amount}. New balance is {self.balance}.")
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient balance.")
+        else:
+            self.balance -= amount
+            print(f"Withdrew {amount}. New balance is {self.balance}.")
