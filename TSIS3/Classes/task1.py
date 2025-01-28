cf3e52426f712dec242870de4ee1f0da8169dfb3
K1rito384 <akeshubaev2006@gmail.com>
2025-01-28 13:49:54 +0500

    new commit

---
+++ TSIS3/Classes/task1.py
@@ -0,0 +1,10 @@
+import math
+class StringProcessor:
+    def __init__(self):
+        self.string = ""
+
+    def getString(self):
+        self.string = input("Enter a string: ")
+
+    def printString(self):
+        print(self.string.upper())
