2c06863d6283b23b39b1a2cc6a21cfe45e672a9b
K1rito384 <akeshubaev2006@gmail.com>
2025-02-12 22:15:54 +0500

    new commit

---
+++ TSIS4/math/parallelogram.py
@@ -0,0 +1,10 @@
+def parallelogram_area(base, height):
+    return base * height
+
+base = 5
+height = 6
+area = parallelogram_area(base, height)
+
+print(f"Length of base: {base}")
+print(f"Height of parallelogram: {height}")
+print(f"Expected Output: {area:.1f}")
